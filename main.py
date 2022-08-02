17a56e59842b49743b554a12e696ad1e98ea1e0c
xJustCallMeAlex <alexander.kolar1379@gmail.com>
2022-08-02 15:43:34 +0200

    added heroku support

--- main.py
+++ main.py
@@ -1,11 +1,50 @@
 import discord
 import os
+import logging
 
 client = discord.Client()
+
+
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[00;20m"
+    green = "\x1b[32;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+
+    err_format = "%(levelname)s: %(asctime)s - %(message)-90s"
+    format = err_format + " \t[%(name)s] (in: %(filename)s, line %(lineno)d)"
+    date_format = "%Y-%m-%d %H:%M:%S"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: green + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + err_format + reset,
+        logging.CRITICAL: bold_red + err_format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, datefmt=self.date_format)
+        return formatter.format(record)
+
+
+def __init_logs():
+    handler = logging.StreamHandler()
+    handler.setFormatter(CustomFormatter())
+
+    logs = logging.getLogger('discord')
+    logs.addHandler(handler)
+    logs.setLevel(logging.INFO)
+    return logs
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -31,4 +70,7 @@ async def on_message(message):
         elif user_message.startswith('!help'):
             await message.channel.send(f'My commands are !add, !remove, !list')
             return
+
+
+logger = __init_logs()
 client.run(os.environ.get("TOKEN", None))
