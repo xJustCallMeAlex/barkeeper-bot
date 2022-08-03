f1457027eaf30c1ca87b1c3a8018124b3d1707ec
xJustCallMeAlex <alexander.kolar1379@gmail.com>
2022-08-03 14:03:50 +0200

    added heroku support

--- main.py
+++ main.py
@@ -1,10 +1,120 @@
+import asyncio
+import uuid
+
 import discord
 import os
 import logging
+from discord.ext import commands
+import asyncpg
+from dataclasses import dataclass
 
-client = discord.Client()
+
+def is_valid(ctx):  # later check channel
+    return ctx.channel.name == 'bot-text'
 
 
+async def run():
+    db = await asyncpg.create_pool(os.environ.get("DATABASE_URL", None), max_size=5, min_size=3)
+    bot = Bot(db)
+    await bot.start(os.environ.get("TOKEN", None))
+
+
+class Bot(commands.Bot):
+    def __init__(self, db):
+        super().__init__(command_prefix='???.?')
+        self.db = db
+
+    # @client.event
+    async def on_ready(self):
+        print('We have logged in as {0.user}'.format(self))
+
+    async def on_message(self, message):
+        await self.process_commands(message)
+        username = str(message.author).split('#')[0]
+        user_message = str(message.content)
+        if message.channel.name == 'bot-text':
+            if user_message.startswith('!add:'):
+                user_message = user_message.split(':')[1]
+                user_message = user_message.strip()
+                await self.db.execute(f"""
+                               INSERT INTO drinks(drink_id, name) VALUES (gen_random_uuid(), '{user_message}');
+                           """)
+                await message.channel.send(f'{user_message} has been added to the shelf by {username}')
+                return
+            elif user_message.startswith('!remove:'):
+                user_message = user_message.split(':')[1]
+                user_message = user_message.strip()
+                await self.db.execute(f"""
+                                DELETE FROM drinks WHERE name = '{user_message}';
+                            """)
+                await message.channel.send(f'{user_message} has been removed from the shelf by {username}')
+                return
+            elif user_message.startswith('!list'):
+                result_set = await self.db.fetch(f"""
+                                SELECT drink_id, name
+                                FROM drinks
+                                ORDER BY drink_id;
+                            """)
+                drink_list = [str(Drink(*result)) for result in result_set]
+                await message.channel.send('This is a list of all the Bottles')
+                await message.channel.send("\n".join(drink_list))
+                return
+            elif user_message.startswith('!help'):
+                await message.channel.send("""
+This is a list of all the commands.
+!add: bottlename - This command adds a bottle to the shelf.
+!remove: bottlename - This command removes a bottle from the shelf. 
+!list - This command lists the whole content of the shelf. Id plus name.
+!help - This command shows the help menu
+
+Important is that the bottlename does not contain ':'. 
+Because the colon is used to split the message string.
+
+This bot was programmed by Alexander Kolar with the help of Johannes Riedmann.
+I hope you enjoy!
+                        """)
+                return
+        print(message.content)
+
+    async def add(self, ctx, message):
+        if is_valid(ctx):
+            await self.db.execute(f"""
+                INSERT INTO drinks(drink_id, name) VALUES (gen_random_uuid(), '{message}');
+            """)
+            await ctx.send(f'{message} has been added to the shelf by {ctx.author}')
+
+    async def remove(self, ctx, message):
+        if is_valid(ctx):
+            await self.db.execute(f"""
+                DELETE FROM drinks WHERE name = '{message}';
+            """)
+            await ctx.send(f'{message} has been removed from the shelf by {ctx.author}')
+
+    async def content(self, ctx):
+        if is_valid(ctx):
+            result_set = await self.db.fetch(f"""
+                SELECT drink_id, name
+                FROM drinks
+                ORDER BY drink_id;
+            """)
+            drink_list = [str(Drink(*result)) for result in result_set]
+            await ctx.send('This is a list of all the Bottles')
+            await ctx.send("\n".join(drink_list))
+
+    async def summarize(self, ctx):
+        await ctx.send("""
+            This is a list of all the commands.
+            !add "bottlename"   - This command adds a bottle to the shelf. You need to put the bottlename in quotation marks.
+            !remove "bottlename"     - This command removes a bottle from the shelf. You need to put the bottlename in quotation marks.
+            !list                       - This command lists the whole content of the shelf. Id plus name.
+            !help                     - This command shows the help menu
+
+            This bot was programmed by Alexander Kolar with the help of Johannes Riedmann.
+            I hope you enjoy!
+        """)
+
+
+# await
 class CustomFormatter(logging.Formatter):
     grey = "\x1b[00;20m"
     green = "\x1b[32;20m"
@@ -41,39 +151,15 @@ def __init_logs():
     return logs
 
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+@dataclass(frozen=True)
+class Drink:
+    drink_id: uuid.UUID
+    name: str
+
+    def __str__(self):
+        return self.name
 
 
-@client.event
-async def on_message(message):
-    username = str(message.author).split('#')[0]
-    user_message = str(message.content)
-    user_message = user_message.lower()
-    channel = str(message.channel.name)
-    print(f'{username}: {user_message} ({channel})')
-
-    if message.author == client.user:
-        return
-
-    await message.channel.send('Hello')
-
-    if message.channel.name == 'bot-text':
-        if user_message.startswith('!add'):
-            await message.channel.send(f'I added the bottle for {username}!')
-            return
-        elif user_message.startswith('!remove'):
-            await message.channel.send(f'I removed the bottle for {username}!')
-            return
-        elif user_message.startswith('!list'):
-            await message.channel.send(f'Here are all the bottles!')
-            return
-        elif user_message.startswith('!help'):
-            await message.channel.send(f'My commands are !add, !remove, !list')
-            return
-
-print('Test Hallo Hallo Blöd')
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
 logger = __init_logs()
-
-client.run(os.environ.get("TOKEN", None))
